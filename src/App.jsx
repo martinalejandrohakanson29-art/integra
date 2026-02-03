bc4ff40ac0f6ce7bf7c0e5bc9b12f79661884bad
martinalejandrohakanson29-art <martinalejandrohakanson29@gmail.com>
2026-02-02 23:42:37 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import MainLayout from './components/MainLayout';
 import LoginPage from './pages/LoginPage';
 import AgendaPage from './pages/AgendaPage';
 import PatientListPage from './pages/PatientListPage';
@@ -12,20 +11,16 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* El Login va solo, sin panel lateral */}
         <Route path="/login" element={<LoginPage />} />
-
-        {/* Todas estas rutas van DENTRO del MainLayout automáticamente */}
-        <Route element={<MainLayout />}>
-          <Route path="/agenda" element={<AgendaPage />} />
-          <Route path="/pacientes" element={<PatientListPage />} />
-          <Route path="/pacientes/nuevo" element={<NewPatientPage />} />
-          <Route path="/pacientes/:id/historia" element={<PatientHistoryPage />} />
-          <Route path="/turnos/:id/editar" element={<EditAppointmentPage />} />
-          
-          {/* Si entras a la raíz, te manda a la agenda */}
-          <Route path="/" element={<Navigate to="/agenda" replace />} />
-        </Route>
+        
+        {/* Rutas individuales: Cada una maneja su propio Layout */}
+        <Route path="/agenda" element={<AgendaPage />} />
+        <Route path="/pacientes" element={<PatientListPage />} />
+        <Route path="/nuevo-paciente" element={<NewPatientPage />} />
+        <Route path="/pacientes/:id/historia" element={<PatientHistoryPage />} />
+        <Route path="/editar-turno" element={<EditAppointmentPage />} />
+        
+        <Route path="/" element={<Navigate to="/agenda" replace />} />
       </Routes>
     </Router>
   );
