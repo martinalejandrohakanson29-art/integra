be3637592d70f3ea433b1ab0ecd64fd88c3828da
martinalejandrohakanson29-art <martinalejandrohakanson29@gmail.com>
2026-02-10 23:21:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from './pages/LoginPage';
 import AgendaPage from './pages/AgendaPage';
 import PatientListPage from './pages/PatientListPage';
 import NewPatientPage from './pages/NewPatientPage';
+import EditPatientPage from './pages/EditPatientPage'; // Nueva Importación
 import PatientHistoryListPage from './pages/PatientHistoryListPage';
 import PatientHistoryPage from './pages/PatientHistoryPage';
 import EditAppointmentPage from './pages/EditAppointmentPage';
@@ -13,20 +14,13 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        
-        {/* Rutas de la aplicación */}
         <Route path="/agenda" element={<AgendaPage />} />
         <Route path="/pacientes" element={<PatientListPage />} />
         <Route path="/nuevo-paciente" element={<NewPatientPage />} />
-        
-        {/* Nueva ruta para el buscador general de historias */}
+        <Route path="/pacientes/:id/editar" element={<EditPatientPage />} /> {/* Nueva Ruta */}
         <Route path="/historia-clinica" element={<PatientHistoryListPage />} />
-        
-        {/* Ruta para la ficha específica de un paciente */}
         <Route path="/pacientes/:id/historia" element={<PatientHistoryPage />} />
-        
         <Route path="/editar-turno" element={<EditAppointmentPage />} />
-        
         <Route path="/" element={<Navigate to="/agenda" replace />} />
       </Routes>
     </Router>
