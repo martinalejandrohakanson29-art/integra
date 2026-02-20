be04ea5eaa84bc717b3ed262007aa002b6ea25f7
martinalejandrohakanson29-art <martinalejandrohakanson29@gmail.com>
2026-02-19 21:48:46 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import LoginPage from './pages/LoginPage';
 import AgendaPage from './pages/AgendaPage';
 import PatientListPage from './pages/PatientListPage';
 import NewPatientPage from './pages/NewPatientPage';
-import EditPatientPage from './pages/EditPatientPage'; // Nueva Importación
+import EditPatientPage from './pages/EditPatientPage';
 import PatientHistoryListPage from './pages/PatientHistoryListPage';
 import PatientHistoryPage from './pages/PatientHistoryPage';
 import EditAppointmentPage from './pages/EditAppointmentPage';
+
+// NUEVAS PÁGINAS DE PROFESIONALES
+import ProfessionalListPage from './pages/ProfessionalListPage';
+import NewProfessionalPage from './pages/NewProfessionalPage';
+import EditProfessionalPage from './pages/EditProfessionalPage';
 
 function App() {
   return (
@@ -15,12 +20,20 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/agenda" element={<AgendaPage />} />
+        
         <Route path="/pacientes" element={<PatientListPage />} />
         <Route path="/nuevo-paciente" element={<NewPatientPage />} />
-        <Route path="/pacientes/:id/editar" element={<EditPatientPage />} /> {/* Nueva Ruta */}
+        <Route path="/pacientes/:id/editar" element={<EditPatientPage />} />
+        
         <Route path="/historia-clinica" element={<PatientHistoryListPage />} />
         <Route path="/pacientes/:id/historia" element={<PatientHistoryPage />} />
         <Route path="/editar-turno" element={<EditAppointmentPage />} />
+
+        {/* RUTAS DE PROFESIONALES */}
+        <Route path="/profesionales" element={<ProfessionalListPage />} />
+        <Route path="/nuevo-profesional" element={<NewProfessionalPage />} />
+        <Route path="/profesionales/:id/editar" element={<EditProfessionalPage />} />
+
         <Route path="/" element={<Navigate to="/agenda" replace />} />
       </Routes>
     </Router>
